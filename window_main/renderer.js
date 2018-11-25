33950e4abaddd74f523480a595f81f4d3e151ee7
Manuel Etchegaray <manuel.etchegaray7@gmail.com>
2018-11-24 23:36:11 -0300

    no log dialog click

--- window_main/renderer.js
+++ window_main/renderer.js
@@ -440,12 +440,12 @@ ipc.on('no_log', function (event, arg) {
 		$('.dialog').css('top', 'calc(50% - 100px)');
 
 		$('.dialog_wrapper').on('click', function(e) {
-			logDialogOpen = false;
 			console.log('.dialog_wrapper on click')
 			//e.stopPropagation();
 			$('.dialog_wrapper').css('opacity', 0);
 			$('.dialog_wrapper').css('pointer-events', 'none');
 			setTimeout(function() {
+				logDialogOpen = false;
 				$('.dialog_wrapper').hide();
 				$('.dialog').css('width', '500px');
 				$('.dialog').css('height', '160px');
@@ -465,7 +465,7 @@ ipc.on('no_log', function (event, arg) {
 
 		cont.append('<div class="share_title">Enter output_log.txt location:</div>');
 		var icd = $('<div class="share_input_container"></div>');
-		var sin = $('<input style="border-radius: 3px; height: 28px;font-size: 14px;" id="log_input" onClick="this.setSelectionRange(0, this.value.length)" autofocus autocomplete="off" value="'+arg+'" />');
+		var sin = $('<input style="border-radius: 3px; height: 28px;font-size: 14px;" id="log_input" autofocus autocomplete="off" value="'+arg+'" />');
 		var but = $('<div class="button_simple">Save</div>');
 		
 		sin.appendTo(icd);
@@ -476,6 +476,17 @@ ipc.on('no_log', function (event, arg) {
 
 		but.click(function () {
 		    ipc_send('set_log', document.getElementById("log_input").value);
+			console.log('.dialog_wrapper on click')
+			//e.stopPropagation();
+			$('.dialog_wrapper').css('opacity', 0);
+			$('.dialog_wrapper').css('pointer-events', 'none');
+			setTimeout(function() {
+				logDialogOpen = false;
+				$('.dialog_wrapper').hide();
+				$('.dialog').css('width', '500px');
+				$('.dialog').css('height', '160px');
+				$('.dialog').css('top', 'calc(50% - 80px)');
+			}, 250);
 		});
 	}
 });
