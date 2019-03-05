f6558870067a8fdc5696bfeed3bb487391b30155
Manuel Etchegaray <manuel.etchegaray7@gmail.com>
2019-03-05 16:50:20 -0300

    redo buffer.from

--- window_background/background.js
+++ window_background/background.js
@@ -1132,7 +1132,7 @@ function decodePayload(json) {
   const messages = require("./messages_pb");
 
   const msgType = json.clientToMatchServiceMessageType.split("_")[1],
-    binaryMsg = new Buffer(json.payload, "base64");
+    binaryMsg = new Buffer.from(json.payload, "base64");
 
   try {
     let msgDeserialiser;
