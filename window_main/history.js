69600cdbce09f26a41747f8c465400ed89a70bfa
Manuel Etchegaray <manuel.etchegaray7@gmail.com>
2019-03-24 11:50:33 -0300

    overflow history text

--- window_main/history.js
+++ window_main/history.js
@@ -177,6 +177,8 @@ function open_history_tab(loadMore) {
     let fltl = createDivision(["flex_item"]);
     let fll = createDivision(["flex_item"]);
     fll.style.flexDirection = "column";
+    fll.style.overflow = "hidden";
+
     let flt = createDivision(["flex_top"]);
     let flb = createDivision(["flex_bottom"]);
     fll.appendChild(flt);
@@ -186,6 +188,7 @@ function open_history_tab(loadMore) {
     let flc = createDivision(["flex_item"]);
     flc.style.flexDirection = "column";
     flc.style.flexGrow = 2;
+    flc.style.overflow = "hidden";
     flc.appendChild(fct);
 
     let fcb = createDivision(["flex_bottom"]);
