06c9673c9c5bc804887b2124f82ca2ac1b4574e1
Manuel Etchegaray <manuel.etchegaray7@gmail.com>
2019-05-12 20:16:07 -0300

    list-item in decks

--- window_main/decks.js
+++ window_main/decks.js
@@ -2,9 +2,8 @@
 global
   Aggregator,
   allMatches,
-	cardsDb,
   createDivision,
-	decks,
+  decks,
   economyHistory,
   FilterPanel,
   formatPercent,
@@ -13,15 +12,16 @@ global
   getReadableFormat,
   hideLoadingBars,
   getWinrateClass,
-	ipc_send,
+  ipc_send,
   makeResizable,
   mana,
-	openDeck,
+  ListItem,
+  openDeck,
   orderedCardRarities,
-	sidebarActive,
+  sidebarActive,
   sidebarSize,
-	sort_decks,
-	getTagColor,
+  sort_decks,
+  getTagColor,
   setTagColor,
   StatsPanel
 */
@@ -38,10 +38,14 @@ function openDecksTab() {
     mainDiv.innerHTML = "";
 
     const wrap_r = createDivision(["wrapper_column", "sidebar_column_l"]);
-    wrap_r.style.width = sidebarSize+"px";
+    wrap_r.style.width = sidebarSize + "px";
     wrap_r.style.flex = `0 0 ${sidebarSize}px`;
     const aggregator = new Aggregator(filters);
-    const statsPanel = new StatsPanel("decks_top", aggregator.stats, sidebarSize);
+    const statsPanel = new StatsPanel(
+      "decks_top",
+      aggregator.stats,
+      sidebarSize
+    );
     const decks_top_winrate = statsPanel.render();
     decks_top_winrate.style.display = "flex";
     decks_top_winrate.style.flexDirection = "column";
@@ -109,56 +113,34 @@ function openDecksTab() {
         aggregator.deckLastPlayed[deck.id]);
 
     decks.filter(isDeckVisible).forEach(deck => {
-      var tileGrpid = deck.deckTileId;
-
-      if (cardsDb.get(tileGrpid).set == undefined) {
-        tileGrpid = 67003;
+      let tileGrpid = deck.deckTileId;
+      let listItem;
+      if (deck.custom) {
+        listItem = new ListItem(
+          tileGrpid,
+          deck.id,
+          openDeckCallback,
+          deleteDeckCallback
+        );
+      } else {
+        listItem = new ListItem(tileGrpid, deck.id, openDeckCallback);
       }
+      listItem.center.classList.add("deck_tags_container");
+      listItem.divideLeft();
+      listItem.divideRight();
 
-      var tile = document.createElement("div");
-      tile.classList.add(deck.id + "t");
-      tile.classList.add("deck_tile");
-
-      try {
-        tile.style.backgroundImage =
-          "url(https://img.scryfall.com/cards" +
-          cardsDb.get(tileGrpid).images["art_crop"] +
-          ")";
-      } catch (e) {
-        console.error(e);
-      }
-
-      var div = document.createElement("div");
-      div.classList.add(deck.id);
-      div.classList.add("list_deck");
-
-      var fll = document.createElement("div");
-      fll.classList.add("flex_item");
-
-      var flc = document.createElement("div");
-      flc.classList.add("flex_item");
-      flc.style.flexDirection = "column";
-      flc.style.whiteSpace = "nowrap";
-
-      var flcf = document.createElement("div");
-      flcf.classList.add("deck_tags_container");
-
-      var flcfwc = document.createElement("div");
-      flcfwc.style.marginRight = "8px";
-      flcfwc.classList.add("flex_item");
-
-      const t = createTag(null, flcf, false);
+      const t = createTag(null, listItem.center, false);
       jQuery.data(t, "deck", deck.id);
       if (deck.format) {
         const fText = getReadableFormat(deck.format);
-        const t = createTag(fText, flcf, false);
+        const t = createTag(fText, listItem.center, false);
         t.style.fontStyle = "italic";
         jQuery.data(t, "deck", deck.id);
       }
       if (deck.tags) {
         deck.tags.forEach(tag => {
           if (tag !== deck.format) {
-            const t = createTag(tag, flcf);
+            const t = createTag(tag, listItem.center);
             jQuery.data(t, "deck", deck.id);
           }
         });
@@ -188,7 +170,9 @@ function openDecksTab() {
             (ownedWildcards[cardRarity] > 0
               ? ownedWildcards[cardRarity] + "/"
               : "") + missingWildcards[cardRarity];
-          flcfwc.appendChild(wc);
+          listItem.right.appendChild(wc);
+          listItem.right.style.flexDirection = "row";
+          listItem.right.style.marginRight = "16px";
         }
       });
       if (n !== 0) {
@@ -196,116 +180,67 @@ function openDecksTab() {
         bo.classList.add("bo_explore_cost");
         bo.innerHTML = Math.round(boosterCost);
         bo.title = "Boosters needed (estimated)";
-        flcfwc.appendChild(bo);
+        listItem.right.appendChild(bo);
       }
-
-      var flr = document.createElement("div");
-      flr.classList.add("flex_item");
-      flr.style.flexDirection = "column";
-
-      var flt = document.createElement("div");
-      flt.classList.add("flex_top");
-
-      var flb = document.createElement("div");
-      flb.classList.add("flex_bottom");
 
       if (deck.name.indexOf("?=?Loc/Decks/Precon/") != -1) {
         deck.name = deck.name.replace("?=?Loc/Decks/Precon/", "");
       }
 
-      d = document.createElement("div");
-      d.classList.add("list_deck_name");
-      d.innerHTML = deck.name;
-      flt.appendChild(d);
+      let deckNameDiv = createDivision(["list_deck_name"], deck.name);
+      listItem.leftTop.appendChild(deckNameDiv);
 
       deck.colors.forEach(function(color) {
-        var d = document.createElement("div");
-        d.classList.add("mana_s20");
-        d.classList.add("mana_" + mana[color]);
-        flb.appendChild(d);
+        let m = createDivision(["mana_s20", "mana_" + mana[color]]);
+        listItem.leftBottom.appendChild(m);
       });
 
       const dwr = aggregator.deckWinrates[deck.id];
       if (dwr && dwr.total > 0) {
-        var d = document.createElement("div");
-        d.classList.add("list_deck_winrate");
-
+        let deckWinrateDiv = createDivision(["list_deck_winrate"]);
         let colClass = getWinrateClass(dwr.winrate);
-        d.innerHTML = `${dwr.wins}:${
+        deckWinrateDiv.innerHTML = `${dwr.wins}:${
           dwr.losses
-        } <span class="${colClass}_bright">(${formatPercent(dwr.winrate)})</span>`;
-        d.title = `${dwr.wins} matches won : ${dwr.losses} matches lost`;
-        flr.appendChild(d);
+        } <span class="${colClass}_bright">(${formatPercent(
+          dwr.winrate
+        )})</span>`;
+        deckWinrateDiv.title = `${dwr.wins} matches won : ${
+          dwr.losses
+        } matches lost`;
+        listItem.rightTop.appendChild(deckWinrateDiv);
 
-        d = document.createElement("div");
-        d.classList.add("list_deck_winrate");
-        d.style.opacity = 0.6;
-
-        d.innerHTML = "Since last edit: ";
+        let deckWinrateLastDiv = createDivision(["list_deck_winrate"]);
+        deckWinrateLastDiv.style.opacity = 0.6;
+        deckWinrateLastDiv.innerHTML = "Since last edit: ";
         const drwr = aggregator.deckRecentWinrates[deck.id];
         if (drwr && drwr.total > 0) {
           colClass = getWinrateClass(drwr.winrate);
-          d.innerHTML += `<span class="${colClass}_bright">${formatPercent(drwr.winrate)}</span>`;
-          d.title = `${drwr.wins} matches won : ${drwr.losses} matches lost`;
+          deckWinrateLastDiv.innerHTML += `<span class="${colClass}_bright">${formatPercent(
+            drwr.winrate
+          )}</span>`;
+          deckWinrateLastDiv.title = `${drwr.wins} matches won : ${
+            drwr.losses
+          } matches lost`;
         } else {
-          d.innerHTML += "<span>--</span>";
-          d.title = "no data yet";
+          deckWinrateLastDiv.innerHTML += "<span>--</span>";
+          deckWinrateLastDiv.title = "no data yet";
         }
-        flr.appendChild(d);
+        listItem.rightBottom.appendChild(deckWinrateLastDiv);
       }
 
-      if (deck.custom) {
-        var fldel = document.createElement("div");
-        fldel.classList.add("flex_item");
-        fldel.classList.add(deck.id + "_del");
-        fldel.classList.add("delete_item");
-      }
-
-      div.appendChild(fll);
-      fll.appendChild(tile);
-      div.appendChild(flc);
-      div.appendChild(flcf);
-      div.appendChild(flcfwc);
-      flc.appendChild(flt);
-      flc.appendChild(flb);
-      div.appendChild(flr);
-      if (deck.custom) {
-        div.appendChild(fldel);
-      }
-
-      wrap_l.appendChild(div);
-
-      $("." + deck.id).on("mouseenter", function() {
-        $("." + deck.id + "t").css("opacity", 1);
-        $("." + deck.id + "t").css("width", "200px");
-      });
-
-      $("." + deck.id).on("mouseleave", function() {
-        $("." + deck.id + "t").css("opacity", 0.66);
-        $("." + deck.id + "t").css("width", "128px");
-      });
-
-      $("." + deck.id).on("click", function() {
-        openDeck(deck, 2);
-        $(".moving_ux").animate({ left: "-100%" }, 250, "easeInOutCubic");
-      });
-
-      if (deck.custom) {
-        deleteDeck(deck);
-      }
+      wrap_l.appendChild(listItem.container);
     });
-
-    $(".delete_item").hover(
-      function() {
-        // in
-        $(this).css("width", "32px");
-      },
-      function() {
-        // out
-        $(this).css("width", "4px");
-      }
-    );
   }
+}
+
+function openDeckCallback(id) {
+  let deck = decks.filter(deck => deck.id == id)[0];
+  openDeck(deck, 2);
+  $(".moving_ux").animate({ left: "-100%" }, 250, "easeInOutCubic");
+}
+
+function deleteDeckCallback(id) {
+  ipc_send("delete_deck", id);
 }
 
 function createTag(tag, div, showClose = true) {
@@ -442,16 +377,6 @@ function deleteTag(deckid, tag) {
 
   let obj = { deck: deckid, name: tag };
   ipc_send("delete_tag", obj);
-}
-
-function deleteDeck(_deck) {
-  $("." + _deck.id + "_del").on("click", function(e) {
-    let currentId = _deck.id;
-    e.stopPropagation();
-    ipc_send("delete_deck", currentId);
-    $("." + currentId).css("height", "0px");
-    $("." + currentId).css("overflow", "hidden");
-  });
 }
 
 module.exports = { openDecksTab: openDecksTab };
