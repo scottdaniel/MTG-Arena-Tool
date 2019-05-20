0a09f9d2ea17b57034707106b8eb33a7a056189a
Benjamin Lu <lu.s.benjamin@gmail.com>
2019-05-20 14:29:35 -0700

    move dateLangs back to background-util

--- window_background/background-util.js
+++ window_background/background-util.js
@@ -1,12 +1,27 @@
 /*
 globals
   _,
-  dateLangs
+  
 */
 // Utility functions that belong only to background
 
 const parse = require("date-fns").parse;
 let logLanguage = "English";
+
+// These were tested briefly , but hey are all taken from actual logs
+// At most some format from date-fns could be wrong;
+// https://date-fns.org/v2.0.0-alpha.7/docs/parse
+let dateLangs = {
+  English: "M/d/yyyy HH:MM:SS A", // ex: 5/19/2019 11:08:01 AM
+  German: "dd.MM.yyyy HH:mm:ss", // ex: 19.05.2019 09:59:00
+  French: "dd/MM/yyyy HH:mm:ss", //ex: 19/05/2019 10:39:42
+  Italian: "dd/MM/yyyy HH:mm:ss", //ex: 19/05/2019 10:43:03
+  Japanese: "yyyy/MM/dd HH:mm:ss", //ex: 2019/05/19 10:45:04
+  Korean: "yyyy-MM-dd A HH:mm:ss", //ex: 2019-05-19 AM 10:56:27
+  "Portugese (Brazil)": "dd/MM/yyyy HH:mm:ss", //ex: 19/05/2019 11:02:32
+  Russian: "dd.MM.yyyy HH:mm:ss", //ex: 19.05.2019 11:05:15
+  Spanish: "dd/MM/yyyy HH:mm:ss" //ex: 19/05/2019 11:06:37
+};
 
 // throws an error if it fails
 function parseWotcTime(dateStr) {
