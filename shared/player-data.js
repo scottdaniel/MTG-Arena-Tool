c41f832c5d856944e74397742bc9fe843813486f
Benjamin Lu <lu.s.benjamin@gmail.com>
2019-06-12 11:16:40 -0700

    player-data reduces tags to case insensitive

--- shared/player-data.js
+++ shared/player-data.js
@@ -183,6 +183,12 @@ class PlayerData {
 
   handleSetData(_event, arg) {
     Object.assign(this, arg);
+    if (arg.tags_colors) {
+      Object.keys(arg.tags_colors).forEach(tag => {
+        const lowerTag = tag.toLowerCase();
+        this.tags_colors[lowerTag] = arg.tags_colors[tag];
+      });
+    }
   }
 
   get cardsSize() {
@@ -233,11 +239,12 @@ class PlayerData {
 
   deck(id) {
     if (!this.deckExists(id)) return false;
+    const tags = (this.decks_tags[id] || []).map(tag => tag.toLowerCase());
     return {
       ...this.decks[id],
       colors: get_deck_colors(this.decks[id]),
       custom: !this.static_decks.includes(id),
-      tags: this.decks_tags[id] || []
+      tags: [...new Set(tags)]
     };
   }
 
@@ -272,7 +279,12 @@ class PlayerData {
 
   match(id) {
     if (!this.matchExists(id)) return false;
-    const match = { ...this[id], type: "match" };
+    const tags = (this[id].tags || []).map(tag => tag.toLowerCase());
+    const match = {
+      ...this[id],
+      tags: [...new Set(tags)],
+      type: "match"
+    };
     if (!match.playerDeck) {
       match.playerDeck = JSON.parse(
         '{"deckTileId":' +
