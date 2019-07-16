4398d43c3539fe1d211d2f5661f8f4a4ad8f7783
Manuel Etchegaray <manuel.etchegaray7@gmail.com>
2019-07-16 20:49:52 -0300

    Update background-util.js

--- window_background/background-util.js
+++ window_background/background-util.js
@@ -19,30 +19,27 @@ const pd = require("../shared/player-data.js");
 // These were tested briefly , but hey are all taken from actual logs
 // At most some format from date-fns could be wrong;
 // https://date-fns.org/v2.0.0-alpha.7/docs/parse
-let dateLangs = {
-  English: "M/dd/yyyy hh:mm:ss aa", //ex 5/28/2019 10:20:35 PM
-  German: "dd.MM.yyyy HH:mm:ss", // ex: 19.05.2019 09:59:00
-  French: "dd/MM/yyyy HH:mm:ss", //ex: 19/05/2019 10:39:42
-  Italian: "dd/MM/yyyy HH:mm:ss", //ex: 19/05/2019 10:43:03
-  Japanese: "yyyy/MM/dd HH:mm:ss", //ex: 2019/05/19 10:45:04
-  Korean: "yyyy-MM-dd A HH:mm:ss", //ex: 2019-05-19 AM 10:56:27
-  "Portugese (Brazil)": "dd/MM/yyyy HH:mm:ss", //ex: 19/05/2019 11:02:32
-  Russian: "dd.MM.yyyy HH:mm:ss", //ex: 19.05.2019 11:05:15
-  Spanish: "dd/MM/yyyy HH:mm:ss" //ex: 19/05/2019 11:06:37
-};
+let dateLangs = [
+  "dd.MM.yyyy HH:mm:ss",
+  "dd/MM/yyyy HH:mm:ss"
+  "M/dd/yyyy hh:mm:ss aa",
+  "dd/MM/yyyy hh:mm:ss aa",
+  "yyyy-MM-dd A HH:mm:ss",
+  "yyyy/MM/dd HH:mm:ss"
+];
 
 // throws an error if it fails
 function parseWotcTime(dateStr) {
-  let date = parse(dateStr, dateLangs[logLanguage], new Date());
+  let date = parse(dateStr, dateLangs[0], new Date());
 
   // This is to detect language when the one read does not match or logLanguage is not yet set
   // Defaults to current time if none matches
   if (!date || isNaN(date.getTime())) {
-    Object.keys(dateLangs).forEach(lang => {
-      let test = parse(dateStr, dateLangs[lang], new Date());
+    dateLangs.forEach(lang => {
+      let test = parse(dateStr, lang, new Date());
       if (test && !isNaN(test.getTime())) {
         //logLanguage = lang;
-        console.log(`Log datetime language detected: ${lang}`, dateStr, test);
+        //console.log(`Log datetime language detected: ${lang}`, dateStr, test);
         date = test;
       }
     });
