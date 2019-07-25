58fdf6d7d62f4365e0b6ff9d1123125364a42878
Benjamin P. McKenna <britishben@gmail.com>
2019-07-25 23:22:21 +0100

    Replace logic of get_collation_set with for loop
    A for loop works out faster than Object.keys().forEach, particularly as it will break as soon as the collationid is found.
    
    add definition, and rename variable from "el" to "name"

--- window_main/economy.js
+++ window_main/economy.js
@@ -112,15 +112,13 @@ function getReadableQuest(questCode) {
 }
 
 //
-function get_colation_set(collationid) {
-  var ret = "";
-  Object.keys(db.sets).forEach(function(setName) {
-    if (db.sets[setName].collation == collationid) {
-      ret = setName;
+function get_collation_set(collationid) {
+  for (let name in db.sets) {
+    if (db.sets[name].collation === collationid) {
+      return name;
     }
-  });
-
-  return ret;
+  }
+  return "";
 }
 
 //
@@ -365,7 +363,7 @@ function createChangeRow(change, economyId) {
 
   if (fullContext === "Booster Open") {
     change.delta.boosterDelta.forEach(function(booster) {
-      var set = get_colation_set(booster.collationId);
+      var set = get_collation_set(booster.collationId);
 
       var bos = createDiv(["set_logo"]);
       bos.style.backgroundImage =
@@ -563,7 +561,7 @@ function createChangeRow(change, economyId) {
 
   if (checkBoosterAdded && change.delta.boosterDelta) {
     change.delta.boosterDelta.forEach(function(booster) {
-      var set = get_colation_set(booster.collationId);
+      var set = get_collation_set(booster.collationId);
 
       var bos = createDiv(["set_logo_med"]);
       bos.style.backgroundImage =
