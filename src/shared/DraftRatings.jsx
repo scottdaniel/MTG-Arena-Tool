f73d41af327e88afaf701c3d583082a8964dab11
Benjamin Lu <lu.s.benjamin@gmail.com>
2019-11-02 11:23:52 -0700

    review feedback

--- src/shared/DraftRatings.jsx
+++ src/shared/DraftRatings.jsx
@@ -1,10 +1,53 @@
 import * as React from "react";
 import { DRAFT_RANKS } from "./constants";
 
+function DraftRankValue(_props) {
+  const { index, rankValue, maxValue } = _props;
+  const rv = 12 - index;
+  // TODO properly type this after #684 lands
+  const rank = DRAFT_RANKS[rv];
+  let colorClass;
+  switch (rank) {
+    case "A+":
+    case "A":
+      colorClass = "blue";
+      break;
+    case "A-":
+    case "B+":
+    case "B":
+      colorClass = "green";
+      break;
+    case "B-":
+    case "C+":
+    case "C":
+    default:
+      colorClass = "white";
+      break;
+    case "C-":
+    case "D+":
+    case "D":
+      colorClass = "orange";
+      break;
+    case "D-":
+    case "F":
+      colorClass = "red";
+      break;
+  }
+  return (
+    <div className="rank_value_container">
+      <div className={"rank_value_title " + colorClass}>{rank}</div>
+      <div
+        className="rank_value_bar"
+        style={{ width: (240 / maxValue) * rankValue + "px" }}
+      />
+    </div>
+  );
+}
+
 export default function DraftRatings(_props) {
   const { card } = _props;
-  const rank = card.rank;
-  const rankValues = card.rank_values;
+  const { rank } = card;
+  const rankValues = card.rank_values || [];
   const rankControversy = card.rank_controversy;
   const maxValue = Math.max(...rankValues);
 
@@ -14,33 +57,14 @@ export default function DraftRatings(_props) {
         Rank: {DRAFT_RANKS[Math.round(rank)]}
       </div>
       <div className="rank_value_container">Controversy: {rankControversy}</div>
-      {rankValues &&
-        rankValues.map((v, index) => {
-          const rv = 12 - index;
-          const rank = DRAFT_RANKS[rv];
-          let colorClass = "white";
-          if (rank == "A+" || rank == "A") {
-            colorClass = "blue";
-          } else if (rank == "A-" || rank == "B+" || rank == "B") {
-            colorClass = "green";
-          } else if (rank == "C-" || rank == "D+" || rank == "D") {
-            colorClass = "orange";
-          } else if (rank == "D-" || rank == "F") {
-            colorClass = "red";
-          }
-          return (
-            <div
-              className="rank_value_container"
-              key={"rank_value_container_" + index}
-            >
-              <div className={"rank_value_title " + colorClass}>{rank}</div>
-              <div
-                className="rank_value_bar"
-                style={{ width: (240 / maxValue) * v + "px" }}
-              />
-            </div>
-          );
-        })}
+      {rankValues.map((rankValue, index) => (
+        <DraftRankValue
+          index={index}
+          key={"rank_value_container_" + index}
+          maxValue={maxValue}
+          rankValue={rankValue}
+        />
+      ))}
     </div>
   );
 }
--- src/shared/OwnershipStars.jsx
+++ src/shared/OwnershipStars.jsx
@@ -3,6 +3,55 @@ import * as React from "react";
 import pd from "./player-data";
 import { cardHasType } from "./card-types";
 
+function OwnershipInfinity(_props) {
+  const { owned, acquired } = _props;
+  let title = "";
+  // Show infinity for basics
+  if (owned > 0) title = `∞ copies in collection`;
+  else title = `0 copies in collection`;
+  if (acquired) {
+    title += ` (∞ recent)`;
+  }
+  let color = "gray";
+  if (owned > 0) color = "green";
+  if (acquired > 0) color = "orange";
+  return <div className={`inventory_card_infinity_${color}`} title={title} />;
+}
+
+function OwnershipStar(_props) {
+  const { owned, acquired, copyIndex, title } = _props;
+  let color = "gray"; // default unowned
+  if (copyIndex < owned) {
+    color = "green"; // owned copy
+  }
+  if (acquired && copyIndex >= owned - acquired && copyIndex < owned) {
+    color = "orange"; // owned and newly acquired copy
+  }
+  return <div className={`inventory_card_quantity_${color}`} title={title} />;
+}
+
+function MultiCardOwnership(_props) {
+  const { owned, acquired } = _props;
+  let title = `${owned || 0}/4 copies in collection`;
+  if (acquired) {
+    title += ` (${acquired} recent)`;
+  }
+  const possibleCopiesIndex = [0, 1, 2, 3];
+  return (
+    <>
+      {possibleCopiesIndex.map(copyIndex => (
+        <OwnershipStar
+          acquired={acquired}
+          copyIndex={copyIndex}
+          key={"inventory_card_quantity_" + copyIndex}
+          owned={owned}
+          title={title}
+        />
+      ))}
+    </>
+  );
+}
+
 export default function OwnershipStars(_props) {
   const { card } = _props;
   if (!card || !card.type) {
@@ -11,41 +60,9 @@ export default function OwnershipStars(_props) {
   const isbasic = cardHasType(card, "Basic Land");
   const owned = pd.cards.cards[card.id];
   const acquired = pd.cardsNew[card.id];
-  let title = "";
-
+  // TODO add custom logic to handle rats and petitioners
   if (isbasic) {
-    // Show infinity for basics (should work for rats and petitioners?)
-    if (owned > 0) title = `∞ copies in collection`;
-    else title = `0 copies in collection`;
-    if (acquired) {
-      title += ` (∞ recent)`;
-    }
-
-    let color = "gray";
-    if (owned > 0) color = "green";
-    if (acquired > 0) color = "orange";
-
-    return <div className={`inventory_card_infinity_${color}`} title={title} />;
+    return <OwnershipInfinity owned={owned} acquired={acquired} />;
   }
-
-  title = `${owned || 0}/4 copies in collection`;
-  if (acquired) {
-    title += ` (${acquired} recent)`;
-  }
-  return (
-    <>
-      {[1, 2, 3, 4].map(i => {
-        let color = "gray";
-        if (i < owned) color = "green";
-        if (acquired && i >= owned - acquired && i < owned) color = "orange";
-        return (
-          <div
-            className={`inventory_card_quantity_${color}`}
-            key={"inventory_card_quantity_" + i}
-            title={title}
-          />
-        );
-      })}
-    </>
-  );
+  return <MultiCardOwnership owned={owned} acquired={acquired} />;
 }
