71998c2bb0e4d79b861031602e90c4b59f270dc3
Thaoden <Thaoden@users.noreply.github.com>
2019-12-21 15:11:01 +0100

    Migrate getNameBySeat to TS (#806)
    * Migrate getNameBySeat to TS
    
    * npm format

---
+++ src/window_background/getNameBySeat.ts
@@ -0,0 +1,31 @@
+import playerData from "../shared/player-data";
+import globals from "./globals";
+
+const suffixLength = "#12345".length;
+
+function getPlayerNameWithoutSuffix(playerName: string) {
+  return playerName.slice(0, -suffixLength);
+}
+
+// Get player name by seat in the game
+const getNameBySeat = function(seat: 1 | 2): string {
+  try {
+    if (seat === globals.currentMatch.player.seat) {
+      return getPlayerNameWithoutSuffix(playerData.name);
+    }
+
+    let oppName = globals.currentMatch.opponent.name;
+    if (!oppName) {
+      return "Opponent";
+    }
+    if (oppName === "Sparky") {
+      return oppName;
+    }
+
+    return getPlayerNameWithoutSuffix(oppName);
+  } catch (e) {
+    return "???";
+  }
+};
+
+export default getNameBySeat;
